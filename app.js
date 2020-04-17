1ae29d96a61f157b8a5487acd8b0ea68f780da06
Khadija Daruwala <khadija.genius@gmail.com>
2020-04-17 15:40:48 +0530

    Render list of items

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inputReason = document.querySelector('#input-reason')
 const inputAmount = document.querySelector('#input-amount')
 const btnCancel = document.querySelector('#btn-cancel')
 const btnConfirm = document.querySelector('#btn-confirm')
+const listExpense = document.querySelector('#expenses-list')
 
 btnConfirm.addEventListener('click', () => {
     const enteredReason = inputReason.value;
@@ -13,5 +14,8 @@ btnConfirm.addEventListener('click', () => {
         enteredAmount.trim().length <= 0) {
         return;
     }
-    console.log(enteredAmount,enteredReason)
+    
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+    listExpense.appendChild(newItem);
 });
